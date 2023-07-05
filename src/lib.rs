3dbecad4ce78686d3cc01726944755dd78f634e2
Michael Yuan <michael@secondstate.io>
2023-07-05 04:31:33 +0000

    Fix issues
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@ impl App {
             return;
         }
 
-        let chat_id = msg.id.to_string();
+        // let chat_id = msg.id.to_string();
         let content = msg.content;
         let channel_id = msg.channel_id;
 
@@ -68,64 +68,64 @@ impl App {
             }
             "/start" => {
                 self.send_msg(channel_id, help_msg).await;
-                store::set(&chat_id.to_string(), json!(true), None);
+                store::set(&channel_id.to_string(), json!(true), None);
                 store::set(
-                    &format!("{}:system_prompt", chat_id),
+                    &format!("{}:system_prompt", channel_id),
                     json!("You are a helpful assistant answering questions on Telegram."),
                     None,
                 );
-                log::info!("Started QA converstion for {}", chat_id);
+                log::info!("Started QA converstion for {}", channel_id);
             }
             "/qa" => {
                 self.send_msg(channel_id, "I am ready for general QA").await;
-                store::set(&chat_id.to_string(), json!(true), None);
+                store::set(&channel_id.to_string(), json!(true), None);
                 store::set(
-                    &format!("{}:system_prompt", chat_id),
+                    &format!("{}:system_prompt", channel_id),
                     json!("You are a helpful assistant answering questions on Telegram."),
                     None,
                 );
-                log::info!("Started QA converstion for {}", chat_id);
+                log::info!("Started QA converstion for {}", channel_id);
             }
             "/summarize" => {
                 self.send_msg(channel_id, "I am ready to summarize text")
                     .await;
-                store::set(&chat_id, json!(true), None);
-                store::set(&format!("{}:system_prompt", chat_id), json!("You are a helpful assistant. Please summarize the next message in short bullet points. Please always answer in English even if the original text is not English."), None);
-                log::info!("Started Chinese translation for {}", chat_id);
+                store::set(&channel_id.to_string(), json!(true), None);
+                store::set(&format!("{}:system_prompt", channel_id), json!("You are a helpful assistant. Please summarize the next message in short bullet points. Please always answer in English even if the original text is not English."), None);
+                log::info!("Started Chinese translation for {}", channel_id);
             }
             "/code" => {
                 self.send_msg(channel_id, "I am ready to review source code")
                     .await;
-                store::set(&chat_id, json!(true), None);
-                store::set(&format!("{}:system_prompt", chat_id), json!("You are an experienced software developer. Please review the computer source code in the next message, explain what it does, and identify potential problems. Please also make suggestions on how to improve it."), None);
-                log::info!("Started code review for {}", chat_id);
+                store::set(&channel_id.to_string(), json!(true), None);
+                store::set(&format!("{}:system_prompt", channel_id), json!("You are an experienced software developer. Please review the computer source code in the next message, explain what it does, and identify potential problems. Please also make suggestions on how to improve it."), None);
+                log::info!("Started code review for {}", channel_id);
             }
             "/translate" => {
                 self.send_msg(channel_id, "I am ready to translate anything into English")
                     .await;
-                store::set(&chat_id, json!(true), None);
-                store::set(&format!("{}:system_prompt", chat_id), json!("You are an English language translator. For every message you receive, please translate it to English. Please respond with just the English translation and nothing more. If the input message is already in English, please fix any grammar errors and improve the writing."), None);
-                log::info!("Started English translation for {}", chat_id);
+                store::set(&channel_id.to_string(), json!(true), None);
+                store::set(&format!("{}:system_prompt", channel_id), json!("You are an English language translator. For every message you receive, please translate it to English. Please respond with just the English translation and nothing more. If the input message is already in English, please fix any grammar errors and improve the writing."), None);
+                log::info!("Started English translation for {}", channel_id);
             }
             "/reply_tweet" => {
                 self.send_msg(channel_id, "I am ready to reply a tweet for you")
                     .await;
-                store::set(&chat_id, json!(true), None);
-                store::set(&format!("{}:system_prompt", chat_id), json!("You are a social media marketing expert. You will receive the text from a tweet. Please generate 3 clever replies to it. Then follow user suggestions to improve the reply tweets."), None);
-                log::info!("Started Twitter marketer for {}", chat_id);
+                store::set(&channel_id.to_string(), json!(true), None);
+                store::set(&format!("{}:system_prompt", channel_id), json!("You are a social media marketing expert. You will receive the text from a tweet. Please generate 3 clever replies to it. Then follow user suggestions to improve the reply tweets."), None);
+                log::info!("Started Twitter marketer for {}", channel_id);
             }
             text => {
                 let placeholder = self.send_msg(channel_id, placeholder_text).await.unwrap();
 
-                let restart = store::get(&chat_id)
+                let restart = store::get(&channel_id.to_string())
                     .and_then(|v| v.as_bool())
                     .unwrap_or(false);
                 if restart {
                     log::info!("Detected restart = true");
-                    store::set(&chat_id, json!(false), None);
+                    store::set(&channel_id.to_string(), json!(false), None);
                 }
 
-                let system_prompt = store::get(&format!("{}:system_prompt", chat_id))
+                let system_prompt = store::get(&format!("{}:system_prompt", channel_id))
                     .and_then(|v| v.as_str().map(|s| s.to_string()))
                     .unwrap_or_default();
 
@@ -138,9 +138,7 @@ impl App {
 
                 if text.is_empty() {
                     log::info!("received empty text");
-
                     let urls = get_image_urls(msg.attachments);
-
                     if urls.is_empty() {
                         log::info!("no image urls");
                         log::debug!("The input message is neither a text nor and image");
@@ -153,6 +151,7 @@ impl App {
                     }
 
                     for url in urls {
+                        log::debug!("Try to DOWNLOAD {}", &url);
                         let bs64 = match download_image(url) {
                             Ok(b) => b,
                             Err(e) => {
@@ -160,6 +159,7 @@ impl App {
                                 continue;
                             }
                         };
+                        log::debug!("Downloaded size {}", bs64.len());
                         let detected = match text_detection(bs64) {
                             Ok(t) => {
                                 log::debug!("text_detection: {}", t);
@@ -172,13 +172,11 @@ impl App {
                             }
                         };
 
-                        self.chat(&chat_id, &detected, &co, channel_id, placeholder.id)
-                            .await;
+                        self.chat(&detected, &co, channel_id, placeholder.id).await;
                     }
                 } else {
                     log::info!("received text: {}", text);
-                    self.chat(&chat_id, text, &co, channel_id, placeholder.id)
-                        .await;
+                    self.chat(text, &co, channel_id, placeholder.id).await;
                 }
             }
         }
@@ -188,13 +186,12 @@ impl App {
 impl App {
     async fn chat(
         &self,
-        chat_id: &str,
         text: &str,
         co: &ChatOptions<'_>,
         channel_id: ChannelId,
         message_id: MessageId,
     ) {
-        match self.openai.chat_completion(chat_id, text, co).await {
+        match self.openai.chat_completion(&channel_id.to_string(), text, co).await {
             Ok(r) => {
                 self.edit_msg(channel_id, message_id, r.choice).await;
             }
